649b48a7460fe6eb2ea652a7caefa154b07fd8a2
Sean Vig <sean.v.775@gmail.com>
2019-09-22 15:29:58 -0400

    Add xcursor manager functionality

--- wlroots/types/xcursor_manager.py
+++ wlroots/types/xcursor_manager.py
@@ -2,6 +2,8 @@
 
 from wlroots import ffi, lib
 
+from .cursor import Cursor
+
 
 class XCursorManager:
     def __init__(self, size, scale=1):
@@ -20,8 +22,20 @@ class XCursorManager:
             ffi.release(self._ptr)
             self._ptr = None
 
+    def set_cursor_image(self, name: str, cursor: Cursor):
+        """Set the cursor image
+
+        Set a Cursor image to the specified cursor name for all scale factors.
+        The wlroots cursor will take over from this point and ensure the
+        correct cursor is used on each output, assuming an output layout is
+        attached to it.
+        """
+        name_cdata = ffi.new("char []", name.encode())
+        lib.wlr_xcursor_manager_set_cursor_image(self._ptr, name_cdata, cursor._ptr)
+
     def __enter__(self):
         """Setup X cursor manager in a context manager"""
+        return self
 
     def __exit__(self, exc_type, exc_value, exc_tb):
         """Clean-up the X cursor manager on contex manager exit"""
