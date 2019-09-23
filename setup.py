51fc23d848b20970159721434465fff6dc69bbde
Sean Vig <sean.v.775@gmail.com>
2019-09-23 08:16:06 -0400

    Move from setup.py to setup.cfg package configuration

--- setup.py
+++ setup.py
@@ -1,50 +1,8 @@
 # Copyright (c) Sean Vig 2018
 
-import sys
-
 from setuptools import setup
 
 
-pywlroots_version = "0.0.1"
-
-description = "Python binding to the wlroots library using cffi"
-
-if "_cffi_backend" in sys.builtin_module_names:
-    import _cffi_backend
-    if _cffi_backend.__version__ < "1.0.0":
-        raise RuntimeError("PyPy version is too old, must support cffi 1.0.0")
-    requires_cffi = "cffi=={}".format(_cffi_backend.__version__)
-else:
-    requires_cffi = "cffi>=1.0.0"
-
-dependencies = ["pywayland", requires_cffi]
-
-classifiers = [
-    "Development Status :: 2 - Pre-Alpha",
-    "Operating System :: POSIX",
-    "Operating System :: POSIX :: Linux",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: Implementation :: CPython",
-    "Programming Language :: Python :: Implementation :: PyPy",
-    "Topic :: Desktop Environment :: Window Managers",
-    "Topic :: Software Development :: Libraries",
-]
-
-modules = [
-    "wlroots",
-]
-
 setup(
-    name="pywlroots",
-    version=pywlroots_version,
-    author="Sean Vig",
-    author_email="sean.v.775@gmail.com",
-    url="https://github.com/flacjacket/pywlroots",
     cffi_modules=["wlroots/ffi_build.py:ffi_builder"],
-    classifiers=classifiers,
-    description=description,
-    install_requires=dependencies,
-    packages=modules,
-    setup_requires=dependencies,
 )
--- wlroots/__init__.py
+++ wlroots/__init__.py
@@ -5,6 +5,8 @@ from ._ffi import ffi, lib  # noqa: F401
 from .backend import Backend  # noqa: F401
 from .renderer import Renderer  # noqa: F401
 
+__version__ = "0.0.1"
+
 __wlroots_version__ = "{}.{}.{}".format(
     lib.WLR_VERSION_MAJOR,
     lib.WLR_VERSION_MICRO,
