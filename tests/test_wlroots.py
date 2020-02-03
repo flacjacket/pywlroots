247c86a07507d4f7dcd25e41ab8c7c76609a24c5
Sean Vig <sean.v.775@gmail.com>
2020-02-03 00:27:02 -0500

    Add pytesting to travis

---
+++ tests/test_wlroots.py
@@ -0,0 +1,6 @@
+from wlroots import ffi, lib
+
+
+def test_it():
+    assert ffi is not None
+    assert lib is not None
--- wlroots/__init__.py
+++ wlroots/__init__.py
@@ -1,12 +1,12 @@
 # Copyright (c) Sean Vig 2018
 
-from ._ffi import ffi, lib  # noqa: F401
-
-from .backend import Backend  # noqa: F401
-from .renderer import Renderer  # noqa: F401
+try:
+    from ._ffi import ffi, lib  # noqa: F401
+except ImportError:
+    __wlroots_version__ = None
+else:
+    __wlroots_version__ = "{}.{}.{}".format(
+        lib.WLR_VERSION_MAJOR, lib.WLR_VERSION_MICRO, lib.WLR_VERSION_MINOR,
+    )
 
 __version__ = "0.0.1"
-
-__wlroots_version__ = "{}.{}.{}".format(
-    lib.WLR_VERSION_MAJOR, lib.WLR_VERSION_MICRO, lib.WLR_VERSION_MINOR,
-)
