bd6c97707e1bcedbb0a0308e8c377aa1567cb917
Sean Vig <sean.v.775@gmail.com>
2020-06-28 11:39:25 -0400

    Fix up package to include headers to build ffi

--- wlroots/__init__.py
+++ wlroots/__init__.py
@@ -1,12 +1,9 @@
 # Copyright (c) Sean Vig 2018
 
-try:
-    from ._ffi import ffi, lib  # noqa: F401
-except ImportError:
-    __wlroots_version__ = None
-else:
-    __wlroots_version__ = "{}.{}.{}".format(
-        lib.WLR_VERSION_MAJOR, lib.WLR_VERSION_MICRO, lib.WLR_VERSION_MINOR,
-    )
+from ._ffi import ffi, lib  # noqa: F401
 
-__version__ = "0.1.0"
+__wlroots_version__ = "{}.{}.{}".format(
+    lib.WLR_VERSION_MAJOR, lib.WLR_VERSION_MICRO, lib.WLR_VERSION_MINOR,
+)
+
+__version__ = "0.1.1"
