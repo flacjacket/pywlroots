e5140ecfce7222b00179996b2294e292711fef7e
Sean Vig <sean.v.775@gmail.com>
2021-04-16 21:42:41 -0400

    Add information on how to launch tiny compositor
    Closes #3

--- tiny/__main__.py
+++ tiny/__main__.py
@@ -1,3 +1,8 @@
+"""An example basic compositor
+
+To run this example, install the wlroots required dependencies, build the ffi
+bindings (`python wlroots/ffi_build.py`), and launch the main (`python -m tiny`).
+"""
 from __future__ import annotations
 
 import logging
