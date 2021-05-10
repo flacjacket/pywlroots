3a71ae6e6cefa8b66eb80ec5dac228f93e821e08
mcol <mcol@posteo.net>
2021-05-10 23:51:05 +0100

    Let PixmanRegion32 be provided with an existing pointer
    Currently PixmanRegion32 will create a new object when instantiated but
    sometimes we want an instance to represent a pixman_region32_t that
    already exists.

--- wlroots/util/region.py
+++ wlroots/util/region.py
@@ -8,13 +8,16 @@ from wlroots.wlr_types.box import Box
 
 
 class PixmanRegion32(Ptr):
-    def __init__(self) -> None:
+    def __init__(self, ptr=None) -> None:
         """This is a convenience wrapper around pixman_region32_t
 
         :param ptr:
             The pixman_region32_t cdata pointer
         """
-        self._ptr = ffi.new("struct pixman_region32 *")
+        if ptr is None:
+            self._ptr = ffi.new("struct pixman_region32 *")
+        else:
+            self._ptr = ptr
 
     def __enter__(self) -> "PixmanRegion32":
         """Use the pixman_region32 in a context manager"""
