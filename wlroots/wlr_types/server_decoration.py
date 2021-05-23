486569006abb655e40bc819700fe9a838d2c1b10
mcol <mcol@posteo.net>
2021-05-23 12:56:57 +0100

    Implement wlr_server_decoration with bare manager
    This adds an interface to wlr/types/wlr_server_decoration.h. Only the
    core part, the server decoration manager, is added, as this is sufficent
    for most compositor use-cases to allow for clients using this protocol
    to detect whether they should be using server or client-side
    decorations. This wlroots interface is becoming deprecated in a future
    version of wlroots (not necessarily the next version). However, this
    will not happen until GTK, which uses this protocol, updates to the
    alternative xdg_decoration. Until these updates have been done, this
    protocol is useful to keep window decorations consistent.

---
+++ wlroots/wlr_types/server_decoration.py
@@ -0,0 +1,37 @@
+# Copyright (c) Matt Colligan 2021
+#
+# This protocol is obsolete and will be removed in a future version. The recommended
+# replacement is xdg-decoration.
+
+import enum
+from typing import TYPE_CHECKING
+
+from wlroots import PtrHasData, lib
+
+if TYPE_CHECKING:
+    from pywayland.server import Display
+
+
+class ServerDecorationManagerMode(enum.IntEnum):
+    NONE = lib.WLR_SERVER_DECORATION_MANAGER_MODE_NONE
+    CLIENT = lib.WLR_SERVER_DECORATION_MANAGER_MODE_CLIENT
+    SERVER = lib.WLR_SERVER_DECORATION_MANAGER_MODE_SERVER
+
+
+class ServerDecorationManager(PtrHasData):
+    def __init__(self, ptr) -> None:
+        """
+        A decoration negotiation interface which implements the KDE protocol:
+        wlr_server_decoration_manager.
+        """
+        self._ptr = ptr
+
+    @classmethod
+    def create(cls, display: "Display") -> "ServerDecorationManager":
+        """Create a wlr_server_decoration_manager for the given display."""
+        ptr = lib.wlr_server_decoration_manager_create(display._ptr)
+        return cls(ptr)
+
+    def set_default_mode(self, default_mode: ServerDecorationManagerMode) -> None:
+        """Set the default decoration mode for this server"""
+        lib.wlr_server_decoration_manager_set_default_mode(self._ptr, default_mode)
