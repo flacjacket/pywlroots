75053d542ce7584d224a1f41f167100db940800a
Sean Vig <sean.v.775@gmail.com>
2021-07-06 22:17:01 -0400

    Add version check before install

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # Copyright (c) Sean Vig 2018
 
+import sys
 from setuptools import setup
 
+
+sys.path.insert(0, "wlroots")
 
 setup(
     cffi_modules=["wlroots/ffi_build.py:ffi_builder"],
---
+++ wlroots/version.py
@@ -0,0 +1,3 @@
+# Copyright (c) Sean Vig 2021
+
+version = "0.14.0"
