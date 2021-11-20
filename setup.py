6bd78eeaa5a876e2cbf62c3f951ef8510e0b6ffb
Sean Vig <sean.v.775@gmail.com>
2021-11-19 22:36:52 -0500

    Fix installation problems
    Fix up the setup.py file so the cffi package and the required
    cffi-related dependencies are specified as setup_requires. To keep from
    needing to install these packages in all cases, make the setup dynamic
    based on the arguments used to invoke it. This allows running help or
    doing `sdist` without needing the dependencies installed. More can be
    added here if needed, but this should be sufficient for the core CI
    functionality to work.
    
    Fixes #65

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
 # Copyright (c) Sean Vig 2018
 
 import sys
+
+from distutils.command.build import build
 from setuptools import setup
+from setuptools.command.install import install
+
+
+NO_SETUP_REQUIRES_ARGUMENTS = ["--help", "-h", "sdist"]
+
+
+class DummyBuild(build):
+    def run(self):
+        raise RuntimeError("Requested running build")
+
+
+class DummyInstall(install):
+    def run(self):
+        raise RuntimeError("Requested running install")
+
+
+def keywords_with_side_effects(argv):
+    if any(arg in NO_SETUP_REQUIRES_ARGUMENTS for arg in argv):
+        return {"cmdclass": {"build": DummyBuild, "install": DummyInstall}}
+    else:
+        return {
+            "setup_requires": ["cffi>=1.12.0", "pywayland>=0.1.1", "xkbcommon>=0.2"],
+            "cffi_modules": ["wlroots/ffi_build.py:ffi_builder"],
+        }
 
 
 sys.path.insert(0, "wlroots")
 
-setup(
-    cffi_modules=["wlroots/ffi_build.py:ffi_builder"],
-)
+setup(**keywords_with_side_effects(sys.argv))
