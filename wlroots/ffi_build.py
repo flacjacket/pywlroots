71e3d073e5171254985cb7eef7931f1d200e8ee2
Antonín Říha <riha.antonin@outlook.com>
2022-01-31 22:04:19 +0100

    Added support for output power management protocol
    Signed-off-by: Antonín Říha <riha.antonin@outlook.com>

--- wlroots/ffi_build.py
+++ wlroots/ffi_build.py
@@ -950,6 +950,43 @@ void wlr_output_configuration_v1_destroy(
     struct wlr_output_configuration_v1 *config);
 """
 
+# types/wlr_output_powewr_management_v1.h
+CDEF += """
+struct wlr_output_power_manager_v1 {
+    struct wl_global *global;
+    struct wl_list output_powers; // wlr_output_power_v1::link
+
+    struct wl_listener display_destroy;
+
+    struct {
+        struct wl_signal set_mode; // wlr_output_power_v1_set_mode_event
+        struct wl_signal destroy;
+    } events;
+
+    void *data;
+};
+
+struct wlr_output_power_v1 {
+    struct wl_resource *resource;
+    struct wlr_output *output;
+    struct wlr_output_power_manager_v1 *manager;
+    struct wl_list link;
+
+    struct wl_listener output_destroy_listener;
+    struct wl_listener output_commit_listener;
+
+    void *data;
+};
+
+struct wlr_output_power_v1_set_mode_event {
+    struct wlr_output *output;
+    enum zwlr_output_power_v1_mode mode;
+};
+
+struct wlr_output_power_manager_v1 *wlr_output_power_manager_v1_create(
+    struct wl_display *display);
+"""
+
 # types/wlr_pointer.h
 CDEF += """
 struct wlr_pointer {
@@ -2138,6 +2175,7 @@ SOURCE = """
 #include <wlr/types/wlr_output_damage.h>
 #include <wlr/types/wlr_output_layout.h>
 #include <wlr/types/wlr_output_management_v1.h>
+#include <wlr/types/wlr_output_power_management_v1.h>
 #include <wlr/types/wlr_pointer_constraints_v1.h>
 #include <wlr/types/wlr_primary_selection_v1.h>
 #include <wlr/types/wlr_relative_pointer_v1.h>
---
+++ wlroots/wlr_types/output_power_management_v1.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import enum
+
+from pywayland.server import Display, Signal
+
+from wlroots import Ptr, ffi, lib
+
+from .output import Output
+
+
+class OutputPowerManagementV1Mode(enum.IntEnum):
+    OFF = 0
+    ON = 1
+
+
+class OutputPowerV1(Ptr):
+    def __init__(self, ptr) -> None:
+        self._ptr = ffi.cast("struct wlr_output_power_v1 *", ptr)
+
+        self.set_mode_event = Signal(ptr=ffi.addressof(self._ptr.events.set_mode))
+        self.destroy_event = Signal(ptr=ffi.addressof(self._ptr.events.destroy))
+
+    @property
+    def output(self) -> Output:
+        return Output(self._ptr.output)
+
+    @property
+    def manager(self) -> OutputPowerManagerV1:
+        return OutputPowerManagerV1(self._ptr.manager)
+
+
+class OutputPowerManagerV1(Ptr):
+    def __init__(self, display: Display) -> None:
+        self._ptr = lib.wlr_output_power_manager_v1_create(display._ptr)
+
+        self.set_mode_event = Signal(
+            ptr=ffi.addressof(self._ptr.events.set_mode), data_wrapper=OutputPowerV1SetModeEvent
+        )
+
+        self.destroy_event = Signal(ptr=ffi.addressof(self._ptr.events.destroy))
+
+
+class OutputPowerV1SetModeEvent(Ptr):
+    def __init__(self, ptr) -> None:
+        self._ptr = ffi.cast("struct wlr_output_power_v1_set_mode_event *", ptr)
+
+    @property
+    def output(self) -> Output:
+        return Output(self._ptr.output)
+
+    @property
+    def mode(self) -> OutputPowerManagementV1Mode:
+        return OutputPowerManagementV1Mode(self._ptr.mode)
