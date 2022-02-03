54a53c60991e66cc91f819fe4c4df4b177507954
Sean Vig <sean.v.775@gmail.com>
2022-02-03 18:06:39 -0500

    Add functionality to re-generate headers

--- check_headers.py
+++ check_headers.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 import tempfile
 
+INCLUDE_PATH = pathlib.Path(__file__).parent / "wlroots" / "include"
 WAYLAND_PROCOTOLS = [
     "stable/xdg-shell/xdg-shell.xml",
     "unstable/idle-inhibit/idle-inhibit-unstable-v1.xml",
@@ -30,16 +31,25 @@ def get_wayland_protocols_dir() -> pathlib.Path | None:
     return None
 
 
-def main(protocols):
-    for protocol in protocols:
-        if not protocol.exists():
-            raise ValueError(f"Protocol does not exist: {protocol}")
+def header_filename(xml_file: pathlib.Path) -> str:
+    return f"{xml_file.stem}-protocol.h"
 
-    expected_protocol_files = {f"{protocol.stem}-protocol.h" for protocol in protocols}
-    protocol_files = {
-        path.name
-        for path in (pathlib.Path(__file__).parent / "wlroots" / "include").iterdir()
+
+def generate_protocol_header(
+    input_xml: pathlib.Path, output_dir: pathlib.Path
+) -> pathlib.Path:
+    output_path = output_dir / header_filename(input_xml)
+    subprocess.check_output(
+        ["wayland-scanner", "server-header", str(input_xml), str(output_path)]
+    )
+    return output_path
+
+
+def check(protocols: list[pathlib.Path]) -> None:
+    expected_protocol_files = {
+        header_filename(protocol_xml) for protocol_xml in protocols
     }
+    protocol_files = {path.name for path in INCLUDE_PATH.iterdir()}
 
     if expected_protocol_files != protocol_files:
         unexpected_files = list(expected_protocol_files - protocol_files)
@@ -49,24 +59,13 @@ def main(protocols):
         )
 
     for protocol_xml in protocols:
-        protocol_header_file = (
-            pathlib.Path(__file__).parent
-            / "wlroots"
-            / "include"
-            / f"{protocol_xml.stem}-protocol.h"
-        )
+        protocol_header_file = INCLUDE_PATH / header_filename(protocol_xml)
         with protocol_header_file.open() as f:
             protocol_header = f.readlines()
 
         with tempfile.TemporaryDirectory() as output_dir:
-            generated_file = pathlib.Path(output_dir) / protocol_header_file.name
-            subprocess.check_output(
-                [
-                    "wayland-scanner",
-                    "server-header",
-                    str(protocol_xml),
-                    str(generated_file),
-                ]
+            generated_file = generate_protocol_header(
+                protocol_xml, pathlib.Path(output_dir)
             )
             with generated_file.open() as f:
                 generated_header = f.readlines()
@@ -83,12 +82,18 @@ def main(protocols):
                 raise ValueError(f"Mismatch in {protocol_xml.name} in line {i}")
 
 
+def generate(protocols: list[pathlib.Path]) -> None:
+    for protocol_xml in protocols:
+        generate_protocol_header(protocol_xml, INCLUDE_PATH)
+
+
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--wayland-dir", default=get_wayland_protocols_dir(), type=pathlib.Path
     )
     parser.add_argument("--wlroots-dir", required=True, type=pathlib.Path)
+    parser.add_argument("--generate", action="store_true")
     args = parser.parse_args(argv)
 
     if args.wayland_dir is None or not args.wayland_dir.exists():
@@ -98,11 +103,21 @@ def parse_args(argv):
     if not args.wlroots_dir.exists():
         raise ValueError(f"Wlroots directory does not exist: {args.wlroots_dir}")
 
-    return [args.wayland_dir / protocol for protocol in WAYLAND_PROCOTOLS] + [
+    protocols = [args.wayland_dir / protocol for protocol in WAYLAND_PROCOTOLS] + [
         args.wlroots_dir / protocol for protocol in WLROOTS_PROTOCOLS
     ]
 
+    for protocol in protocols:
+        if not protocol.exists():
+            raise ValueError(f"Protocol does not exist: {protocol}")
+
+    return protocols, args.generate
+
 
 if __name__ == "__main__":
-    protocols = parse_args(sys.argv[1:])
-    main(protocols)
+    protocols, generate_headers = parse_args(sys.argv[1:])
+
+    if generate_headers:
+        generate(protocols)
+    else:
+        check(protocols)
