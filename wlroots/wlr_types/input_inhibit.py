f2930bda372b8822ab42c1ae3b952fc754fd93ac
mcol <mcol@posteo.net>
2023-10-04 01:05:54 +0100

    (wlroots 8cfd449) input-inhibitor: deprecate

--- wlroots/wlr_types/input_inhibit.py
+++ wlroots/wlr_types/input_inhibit.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2021 Graeme Holliday
 
+import warnings
+
 from pywayland.server import Client, Display, Signal
 
 from wlroots import Ptr, ffi, lib
@@ -8,6 +10,13 @@ from wlroots import Ptr, ffi, lib
 class InputInhibitManager(Ptr):
     def __init__(self, display: Display) -> None:
         """Creates a wlr_input_inhibit_manager"""
+        warnings.warn(
+            "Following the protocol deprecation, wlr/types/wlr_input_inhibitor.h is"
+            " deprecated and will be removed in the next release.",
+            DeprecationWarning,
+            stacklevel=1,
+        )
+
         self._ptr = lib.wlr_input_inhibit_manager_create(display._ptr)
 
         self.activate_event = Signal(ptr=ffi.addressof(self._ptr.events.activate))
