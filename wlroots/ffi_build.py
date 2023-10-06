530c52745a54f978b3ba68949f200487a08adb2a
mcol <mcol@posteo.net>
2023-10-07 00:23:54 +0100

    (wlroots a289f81) drop KDE idle protocol support

--- wlroots/ffi_build.py
+++ wlroots/ffi_build.py
@@ -744,73 +744,6 @@ struct wlr_gamma_control_manager_v1 *wlr_gamma_control_manager_v1_create(
     struct wl_display *display);
 """
 
-# types/wlr_idle.h
-CDEF += """
-struct wlr_idle {
-    struct wl_global *global;
-    struct wl_list idle_timers; // wlr_idle_timeout::link
-    struct wl_event_loop *event_loop;
-    bool enabled;
-
-    struct wl_listener display_destroy;
-    struct {
-        struct wl_signal activity_notify;
-        struct wl_signal destroy;
-    } events;
-
-    void *data;
-    ...;
-};
-
-struct wlr_idle_timeout {
-    struct wl_resource *resource;
-    struct wl_list link;
-    struct wlr_seat *seat;
-
-    struct wl_event_source *idle_source;
-    bool idle_state;
-    bool enabled;
-    uint32_t timeout; // milliseconds
-
-    struct {
-        struct wl_signal idle;
-        struct wl_signal resume;
-        struct wl_signal destroy;
-    } events;
-
-    struct wl_listener input_listener;
-    struct wl_listener seat_destroy;
-
-    void *data;
-    ...;
-};
-
-struct wlr_idle *wlr_idle_create(struct wl_display *display);
-
-/**
- * Send notification to restart all timers for the given seat. Called by
- * compositor when there is an user activity event on that seat.
- */
-void wlr_idle_notify_activity(struct wlr_idle *idle, struct wlr_seat *seat);
-
-/**
- * Enable or disable timers for a given idle resource by seat.
- * Passing a NULL seat means update timers for all seats.
- */
-void wlr_idle_set_enabled(struct wlr_idle *idle, struct wlr_seat *seat,
-    bool enabled);
-
-/**
- * Create a new timer on the given seat. The idle event will be called after
- * the given amount of milliseconds of inactivity, and the resumed event will
- * be sent at the first user activity after the fired event.
- */
-struct wlr_idle_timeout *wlr_idle_timeout_create(struct wlr_idle *idle,
-    struct wlr_seat *seat, uint32_t timeout);
-
-void wlr_idle_timeout_destroy(struct wlr_idle_timeout *timeout);
-"""
-
 # types/wlr_input_inhibit_v1.h
 CDEF += """
 struct wlr_idle_inhibit_manager_v1 {
@@ -2776,7 +2709,6 @@ SOURCE = """
 #include <wlr/types/wlr_viewporter.h>
 #include <wlr/types/wlr_foreign_toplevel_management_v1.h>
 #include <wlr/types/wlr_gamma_control_v1.h>
-#include <wlr/types/wlr_idle.h>
 #include <wlr/types/wlr_idle_inhibit_v1.h>
 #include <wlr/types/wlr_idle_notify_v1.h>
 #include <wlr/types/wlr_input_inhibitor.h>
