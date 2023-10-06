e87255df7ada8b8a5e085a75b0965e69693d0bd2
mcol <mcol@posteo.net>
2023-10-06 21:44:07 +0100

    (wlroots bd5c4f4) xdg-shell: rework roles

--- wlroots/ffi_build.py
+++ wlroots/ffi_build.py
@@ -2593,6 +2593,7 @@ struct wlr_xdg_surface {
     struct wlr_surface *surface;
     struct wl_list link; // wlr_xdg_client::surfaces
     enum wlr_xdg_surface_role role;
+    struct wl_resource *role_resource;
 
     union {
         struct wlr_xdg_toplevel *toplevel;
@@ -2607,8 +2608,6 @@ struct wlr_xdg_surface {
     struct wl_list configure_list;
 
     struct wlr_xdg_surface_state current, pending;
-
-    struct wl_listener surface_commit;
 
     struct {
         struct wl_signal destroy;
