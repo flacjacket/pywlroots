975f1d708e2d8a638126e2e0c918694e50d23b38
mcol <mcol@posteo.net>
2023-10-06 22:37:36 +0100

    (wlroots 0fdbdc3) xdg-surface: fix init state flow

--- wlroots/ffi_build.py
+++ wlroots/ffi_build.py
@@ -2789,7 +2789,6 @@ struct wlr_xdg_toplevel_requested {
 struct wlr_xdg_toplevel {
     struct wl_resource *resource;
     struct wlr_xdg_surface *base;
-    bool sent_initial_configure;
 
     struct wlr_xdg_toplevel *parent;
     struct wl_listener parent_unmap;
@@ -2852,6 +2851,9 @@ struct wlr_xdg_surface {
     struct wl_list configure_list;
 
     struct wlr_xdg_surface_state current, pending;
+
+    bool initialized;
+    bool initial_commit;
 
     struct {
         struct wl_signal destroy;
