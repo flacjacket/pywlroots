011c3ad6d967d59fa43be14e8846bc23e3356171
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2024-05-10 16:39:30 +0200

    Tiny + Wlroots: Use seat.set_keyboard again

--- tiny/server.py
+++ tiny/server.py
@@ -234,7 +234,7 @@ class TinywlServer:
         self, modifiers: KeyboardModifiers, input_device: InputDevice
     ) -> None:
         keyboard = Keyboard.from_input_device(input_device)
-        self._seat.keyboard = keyboard
+        self._seat.set_keyboard(keyboard)
         self._seat.keyboard_notify_modifiers(modifiers)
 
     def send_key(self, key_event: KeyboardKeyEvent, input_device: InputDevice) -> None:
@@ -260,7 +260,7 @@ class TinywlServer:
 
         # Otherwise, we pass it along to the client
         if not handled:
-            self._seat.keyboard = keyboard
+            self._seat.set_keyboard(keyboard)
             self._seat.keyboard_notify_key(key_event)
 
     def handle_keybinding(self, keysym: int) -> bool:
@@ -419,7 +419,7 @@ class TinywlServer:
         keyboard_handler = KeyboardHandler(keyboard, input_device, self)
         self.keyboards.append(keyboard_handler)
 
-        self._seat.keyboard = keyboard
+        self._seat.set_keyboard(keyboard)
 
     # #############################################################
     # cursor motion callbacks
